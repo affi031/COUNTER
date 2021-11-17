aa6b9965315d5e14d4a39ec202f2f165f8462df3
affi031 <afnanshaikh98@gmail.com>
2021-11-17 21:09:50 +0530

    Counter

---
+++ script.js
@@ -0,0 +1,37 @@
+let counter =0;
+ const value = document.querySelector('#value');
+ const btns = document.querySelectorAll(".btn");
+
+
+ btns.forEach(function (btn){
+     btn.addEventListener("click",function (e){
+         const styles = e.currentTarget.classList;
+         if(styles.contains("decrease"))
+         {
+             counter--;
+
+         }
+         else if(styles.contains("increase"))
+         {
+             counter++;
+         }
+         else
+         {
+             counter=0;
+         }
+
+         if(counter>0)
+         {
+             value.style.color="green";
+         }
+        else if(counter<0)
+         {
+             value.style.color="red";
+         }
+         else
+         {
+             value.style.color="blue";
+         }
+         value.textContent=counter;
+     });
+ });
